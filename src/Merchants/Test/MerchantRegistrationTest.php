83f2abfb8a43dc1211ac02dde2613da40f5ea777
Pascal Thesing <pascal@thesing.me>
2020-03-26 13:01:27 +0100

    Remove sales channel preset

--- src/Merchants/Test/MerchantRegistrationTest.php
+++ src/Merchants/Test/MerchantRegistrationTest.php
@@ -4,7 +4,9 @@ namespace Shopware\Storefront\Test\Controller;
 
 use PHPUnit\Framework\TestCase;
 use Shopware\Core\Defaults;
+use Shopware\Core\Framework\Context;
 use Shopware\Core\Framework\DataAbstractionLayer\EntityRepositoryInterface;
+use Shopware\Core\Framework\DataAbstractionLayer\Search\Criteria;
 use Shopware\Core\Framework\Test\TestCaseBase\IntegrationTestBehaviour;
 use Shopware\Core\Framework\Uuid\Uuid;
 use Shopware\Core\Framework\Validation\DataBag\RequestDataBag;
@@ -44,19 +46,24 @@ class MerchantRegistrationTest extends TestCase
                 'lastName' => 'not',
                 'firstName' => 'First name',
                 'salutationId' => $salutationId,
-                'name' => "foo"
+                'name' => "foo",
+                'salesChannelId' => Defaults::SALES_CHANNEL
             ];
 
         $context = $this->getContainer()->get(SalesChannelContextFactory::class)
             ->create(Uuid::randomHex(), Defaults::SALES_CHANNEL, [SalesChannelContextService::CUSTOMER_ID => $id1]);
 
-        $request = new Request();
-        $request->attributes->set(PlatformRequest::ATTRIBUTE_SALES_CHANNEL_CONTEXT_OBJECT, $context);
-        $this->getContainer()->get('request_stack')->push($request);
-
         $jsonResponse = $this->getContainer()->get(RegistrationController::class)->register(new RequestDataBag($merchantData), $context);
         $result = (array) json_decode($jsonResponse->getContent());
 
         self::assertNotNull($result['data']);
+
+        $result = $this->getContainer()->get('merchant.repository')
+            ->search(new Criteria(), Context::createDefaultContext());
+        self::assertSame(1, $result->count());
+
+        $customerResult = $this->getContainer()->get('customer.repository')
+            ->search(new Criteria(), Context::createDefaultContext());
+        self::assertSame(1, $customerResult->count());
     }
 }
